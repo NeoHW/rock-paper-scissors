a1ac023797fa58d4b9e8b94188d2d14baf63e4fd
NeoHW <neohaowei@gmail.com>
2022-02-21 15:59:47 +0800

    Finished logic and responsive webpage design less game end function

--- script.js
+++ script.js
@@ -1,39 +1,64 @@
-/* assign score variables */ 
+// buttons
+const rockBtn = document.querySelector('#rockButton');
+const paperBtn = document.querySelector('#paperButton');
+const scissorsBtn = document.querySelector('#scissorsButton');
+
+// Divs
+const infoResult = document.querySelector('.info-result');
+const infoReason = document.querySelector('.info-reason');
+
+const playerSign = document.querySelector('#playerSign');
+const playerPoints = document.querySelector('#playerPoints');
+const computerSign = document.querySelector('#computerSign');
+const computerPoints = document.querySelector('#computerPoints');
+
+
+//// Script ////
+
+// assign score variables // 
 let playerScore = 0;
 let computerScore = 0;
 let roundwinner = '';
 
+
 function playRound(playerSelection, computerSelection) {
     /* For Draws */
     if (playerSelection == computerSelection) {
-        console.log("It's a draw!");
+        infoResult.textContent = "It's a draw!"
+        infoReason.textContent = "Same sign ties with each other"
     }
     /* For Computer Wins */
     else if (playerSelection == "rock" && computerSelection == "paper") {
         computerScore++;
-        console.log("You Lose! Paper beats rock");
-        
+        infoResult.textContent = "You Lose!"
+        infoReason.textContent = "Paper beats rock"
     }
     else if (playerSelection == "paper" && computerSelection == "scissors") {
         computerScore++;
-        console.log("You Lose! Scissors beats Paper");
+        infoResult.textContent = "You Lose!"
+        infoReason.textContent = "Scissors beats Paper"
     }
     else if (playerSelection == "scissors" && computerSelection == "rock") {
         computerScore++;
-        console.log("You Lose! Rock beats Scissors");
+        infoResult.textContent = "You Lose!"
+        infoReason.textContent = "Rock beats Scissors"
     }
+
     /* For Player Wins */
     else if (playerSelection == "paper" && computerSelection == "rock") {
         playerScore++;
-        console.log("You Win! Paper beats rock");
+        infoResult.textContent = "You Win!"
+        infoReason.textContent = "Paper beats rock"
     }
     else if (playerSelection == "scissors" && computerSelection == "paper") {
         playerScore++;
-        console.log("You Win! Scissors beats Paper");
+        infoResult.textContent = "You Win!"
+        infoReason.textContent = "Scissors beats Paper"
     }
     else if (playerSelection == "rock" && computerSelection == "scissors") {
         playerScore++;
-        console.log("You Win! Rock beats Scissors");
+        infoResult.textContent = "You Win!"
+        infoReason.textContent = "Rock beats Scissors"
     }
 }
 
@@ -41,12 +66,15 @@ function computerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
 
     if(randomNumber == 0) {
+        computerSign.textContent = "✊"
         return "rock";
     }
     else if (randomNumber == 1) {
+        computerSign.textContent = "✋"
         return "paper";
     }
     else {
+        computerSign.textContent = "✌"
         return "scissors";
     }
 }
@@ -56,8 +84,8 @@ function computerChoice() {
 function game(playerChoice) {
     if (playerScore < 5 && computerScore < 5) {
         playRound(playerChoice, computerChoice());
-        console.log(`Your score: ${playerScore}`);
-        console.log(`Computer's score: ${computerScore}`);
+        playerPoints.textContent = `Your score: ${playerScore}`;
+        computerPoints.textContent = `Computer's score: ${computerScore}`;
     }
 
     else if (playerScore == 5) {
@@ -69,23 +97,20 @@ function game(playerChoice) {
 
 }
 
-
-// UI 
-const rockBtn = document.querySelector('#rockButton');
-const paperBtn = document.querySelector('#paperButton');
-const scissorsBtn = document.querySelector('#scissorsButton');
-
 rockBtn.addEventListener('click', () => {
     // Call the function start with 'rock' as argument 
+    playerSign.textContent = "✊"
     game('rock');
 });
 
 paperBtn.addEventListener('click', () => {
     // Call the function start with 'paper' as argument 
     game('paper');
+    playerSign.textContent = "✋"
 });
 
 scissorsBtn.addEventListener('click', () => {
     // Call the function start with 'scissors' as argument 
     game('scissors');
+    playerSign.textContent = "✌"
 });
